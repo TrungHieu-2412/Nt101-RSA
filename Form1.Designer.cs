c88e5749c6f2ac71340b48657630ccdb4fd19ab4
Hieu <nguyentrunghieu170221@gmail.com>
2025-12-21 13:08:48 +0700

    RSA

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQ = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPublicKey = new System.Windows.Forms.TextBox();
+            this.txtPrivateKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.btnSign = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.btnSecureSend = new System.Windows.Forms.Button();
+            this.btnSecureReceive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số nguyên tố p";
+            // 
+            // txtP
+            // 
+            this.txtP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtP.Location = new System.Drawing.Point(12, 37);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(205, 30);
+            this.txtP.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Số nguyên tố q";
+            // 
+            // txtQ
+            // 
+            this.txtQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQ.Location = new System.Drawing.Point(12, 126);
+            this.txtQ.Name = "txtQ";
+            this.txtQ.Size = new System.Drawing.Size(205, 30);
+            this.txtQ.TabIndex = 3;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(265, 61);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(118, 62);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Tạo Khóa";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(431, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Public Key (n, e)";
+            // 
+            // txtPublicKey
+            // 
+            this.txtPublicKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPublicKey.Location = new System.Drawing.Point(436, 45);
+            this.txtPublicKey.Name = "txtPublicKey";
+            this.txtPublicKey.ReadOnly = true;
+            this.txtPublicKey.Size = new System.Drawing.Size(205, 30);
+            this.txtPublicKey.TabIndex = 6;
+            // 
+            // txtPrivateKey
+            // 
+            this.txtPrivateKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrivateKey.Location = new System.Drawing.Point(436, 126);
+            this.txtPrivateKey.Name = "txtPrivateKey";
+            this.txtPrivateKey.ReadOnly = true;
+            this.txtPrivateKey.Size = new System.Drawing.Size(205, 30);
+            this.txtPrivateKey.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(431, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Private Key (n, d)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Bản rõ (Plaintext)";
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlaintext.Location = new System.Drawing.Point(13, 228);
+            this.txtPlaintext.Multiline = true;
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(628, 391);
+            this.txtPlaintext.TabIndex = 10;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 644);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(124, 53);
+            this.btnEncrypt.TabIndex = 11;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(701, 644);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(124, 53);
+            this.btnDecrypt.TabIndex = 12;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(696, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(187, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Bản mã (Ciphertext)";
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCiphertext.Location = new System.Drawing.Point(701, 228);
+            this.txtCiphertext.Multiline = true;
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(593, 391);
+            this.txtCiphertext.TabIndex = 14;
+            // 
+            // btnSign
+            // 
+            this.btnSign.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSign.Location = new System.Drawing.Point(224, 644);
+            this.btnSign.Name = "btnSign";
+            this.btnSign.Size = new System.Drawing.Size(124, 53);
+            this.btnSign.TabIndex = 15;
+            this.btnSign.Text = "Sign";
+            this.btnSign.UseVisualStyleBackColor = true;
+            this.btnSign.Click += new System.EventHandler(this.btnSign_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerify.Location = new System.Drawing.Point(897, 644);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(124, 53);
+            this.btnVerify.TabIndex = 16;
+            this.btnVerify.Text = "Verify";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // btnSecureSend
+            // 
+            this.btnSecureSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSecureSend.Location = new System.Drawing.Point(436, 644);
+            this.btnSecureSend.Name = "btnSecureSend";
+            this.btnSecureSend.Size = new System.Drawing.Size(205, 53);
+            this.btnSecureSend.TabIndex = 17;
+            this.btnSecureSend.Text = "Sign - Encrypt";
+            this.btnSecureSend.UseVisualStyleBackColor = true;
+            this.btnSecureSend.Click += new System.EventHandler(this.btnSecureSend_Click);
+            // 
+            // btnSecureReceive
+            // 
+            this.btnSecureReceive.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSecureReceive.Location = new System.Drawing.Point(1089, 644);
+            this.btnSecureReceive.Name = "btnSecureReceive";
+            this.btnSecureReceive.Size = new System.Drawing.Size(205, 53);
+            this.btnSecureReceive.TabIndex = 18;
+            this.btnSecureReceive.Text = "Decrypt - Verify";
+            this.btnSecureReceive.UseVisualStyleBackColor = true;
+            this.btnSecureReceive.Click += new System.EventHandler(this.btnSecureReceive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 709);
+            this.Controls.Add(this.btnSecureReceive);
+            this.Controls.Add(this.btnSecureSend);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnSign);
+            this.Controls.Add(this.txtCiphertext);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtPlaintext);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPrivateKey);
+            this.Controls.Add(this.txtPublicKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtQ);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQ;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPublicKey;
+        private System.Windows.Forms.TextBox txtPrivateKey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.Button btnSign;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.Button btnSecureSend;
+        private System.Windows.Forms.Button btnSecureReceive;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        BigInteger n, e_key, d_key;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BigInteger p = BigInteger.Parse(txtP.Text);
+                BigInteger q = BigInteger.Parse(txtQ.Text);
+
+                if (p == q) { MessageBox.Show("p và q không được giống nhau!"); return; }
+
+                n = p * q;
+                BigInteger phi = (p - 1) * (q - 1);
+
+                e_key = ChooseE(phi);
+                d_key = ModInverse(e_key, phi);
+
+                txtPublicKey.Text = $"n = {n}, e = {e_key}";
+                txtPrivateKey.Text = $"n = {n}, d = {d_key}";
+
+                MessageBox.Show("Tạo khóa thành công!", "Thông báo");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi nhập liệu: Hãy nhập số nguyên tố p và q hợp lệ.\n" + ex.Message);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (n == 0) { MessageBox.Show("Vui lòng tạo khóa trước!"); return; }
+
+            string plainText = txtPlaintext.Text;
+            List<string> cipherNumbers = new List<string>();
+
+            foreach (char c in plainText)
+            {
+                BigInteger m = (int)c;
+
+                if (m >= n)
+                {
+                    MessageBox.Show($"Lỗi: Ký tự '{c}' có giá trị {m} lớn hơn n={n}.\nHãy chọn số nguyên tố p, q lớn hơn.");
+                    return;
+                }
+
+                BigInteger encryptedChar = BigInteger.ModPow(m, e_key, n);
+                cipherNumbers.Add(encryptedChar.ToString());
+            }
+
+            txtCiphertext.Text = string.Join(",", cipherNumbers);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (n == 0) { MessageBox.Show("Vui lòng tạo khóa trước!"); return; }
+
+            try
+            {
+                string cipherText = txtCiphertext.Text;
+                string[] numbers = cipherText.Split(',');
+                string decryptedText = "";
+
+                foreach (string numStr in numbers)
+                {
+                    if (string.IsNullOrWhiteSpace(numStr)) continue;
+
+                    BigInteger c = BigInteger.Parse(numStr.Trim());
+                    BigInteger decryptedCharNum = BigInteger.ModPow(c, d_key, n);
+                    decryptedText += (char)decryptedCharNum;
+                }
+
+                txtPlaintext.Text = decryptedText;
+                MessageBox.Show("Giải mã xong: " + decryptedText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi giải mã: Định dạng bản mã không đúng.\n" + ex.Message);
+            }
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            if (n == 0) { MessageBox.Show("Vui lòng tạo khóa trước!"); return; }
+
+            string message = txtPlaintext.Text;
+            if (string.IsNullOrEmpty(message)) { MessageBox.Show("Hãy nhập nội dung cần ký vào ô Bản rõ."); return; }
+
+            List<string> signatureNumbers = new List<string>();
+
+            foreach (char c in message)
+            {
+                BigInteger m = (int)c;
+
+                if (m >= n)
+                {
+                    MessageBox.Show("Lỗi: Khóa n quá nhỏ so với nội dung tin nhắn.");
+                    return;
+                }
+
+                BigInteger s = BigInteger.ModPow(m, d_key, n);
+                signatureNumbers.Add(s.ToString());
+            }
+
+            txtCiphertext.Text = string.Join(",", signatureNumbers);
+            MessageBox.Show("Đã ký tên thành công!");
+        }
+
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            if (n == 0) { MessageBox.Show("Chưa có khóa công khai để xác minh!"); return; }
+            if (string.IsNullOrEmpty(txtCiphertext.Text)) { MessageBox.Show("Không có chữ ký để xác minh."); return; }
+
+            try
+            {
+                string signatureStr = txtCiphertext.Text;
+                string originalMessage = txtPlaintext.Text;
+
+                string[] numbers = signatureStr.Split(',');
+                string decryptedSignature = "";
+
+                foreach (string numStr in numbers)
+                {
+                    if (string.IsNullOrWhiteSpace(numStr)) continue;
+                    BigInteger s = BigInteger.Parse(numStr.Trim());
+
+                    BigInteger m_prime = BigInteger.ModPow(s, e_key, n);
+                    decryptedSignature += (char)m_prime;
+                }
+
+                if (decryptedSignature == originalMessage)
+                {
+                    MessageBox.Show("CHỮ KÝ HỢP LỆ!\nThông điệp chính xác.", "Kết quả xác minh");
+                }
+                else
+                {
+                    MessageBox.Show($"CHỮ KÝ KHÔNG HỢP LỆ!\nNội dung từ chữ ký: {decryptedSignature}\nNội dung gốc: {originalMessage}", "Cảnh báo giả mạo");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xác minh: " + ex.Message);
+            }
+        }
+
+        private void btnSecureSend_Click(object sender, EventArgs e)
+        {
+            if (n == 0) { MessageBox.Show("Vui lòng tạo khóa trước!"); return; }
+
+            string message = txtPlaintext.Text;
+            if (string.IsNullOrEmpty(message)) { MessageBox.Show("Hãy nhập nội dung cần gửi."); return; }
+
+            try
+            {
+                List<string> signatureList = new List<string>();
+                foreach (char c in message)
+                {
+                    BigInteger m = (int)c;
+                    if (m >= n) { MessageBox.Show("Lỗi: Ký tự quá lớn so với n."); return; }
+                    BigInteger s = BigInteger.ModPow(m, d_key, n);
+                    signatureList.Add(s.ToString());
+                }
+                string signatureBlock = string.Join(",", signatureList);
+
+                List<string> cipherList = new List<string>();
+                foreach (char c in message)
+                {
+                    BigInteger m = (int)c;
+                    BigInteger encryptedChar = BigInteger.ModPow(m, e_key, n);
+                    cipherList.Add(encryptedChar.ToString());
+                }
+                string cipherBlock = string.Join(",", cipherList);
+
+                string finalPackage = signatureBlock + "#" + cipherBlock;
+
+                txtCiphertext.Text = finalPackage;
+                MessageBox.Show("Đã Ký và Mã hóa thành công!\nGói tin bao gồm chữ ký và nội dung mật.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+        }
+
+        private void btnSecureReceive_Click(object sender, EventArgs e)
+        {
+            if (n == 0) { MessageBox.Show("Chưa có khóa!"); return; }
+            if (string.IsNullOrEmpty(txtCiphertext.Text)) return;
+
+            try
+            {
+                string package = txtCiphertext.Text;
+
+                string[] parts = package.Split('#');
+                if (parts.Length != 2)
+                {
+                    MessageBox.Show("Gói tin sai định dạng (Thiếu chữ ký hoặc nội dung).");
+                    return;
+                }
+
+                string signatureBlock = parts[0];
+                string cipherBlock = parts[1];
+
+                string[] cipherNumbers = cipherBlock.Split(',');
+                string decryptedMessage = "";
+                foreach (string numStr in cipherNumbers)
+                {
+                    if (string.IsNullOrWhiteSpace(numStr)) continue;
+                    BigInteger c = BigInteger.Parse(numStr.Trim());
+                    BigInteger m = BigInteger.ModPow(c, d_key, n);
+                    decryptedMessage += (char)m;
+                }
+
+                txtPlaintext.Text = decryptedMessage;
+
+                string[] signatureNumbers = signatureBlock.Split(',');
+                string verifiedSignature = "";
+                foreach (string numStr in signatureNumbers)
+                {
+                    if (string.IsNullOrWhiteSpace(numStr)) continue;
+                    BigInteger s = BigInteger.Parse(numStr.Trim());
+                    BigInteger v = BigInteger.ModPow(s, e_key, n);
+                    verifiedSignature += (char)v;
+                }
+
+                if (decryptedMessage == verifiedSignature)
+                {
+                    MessageBox.Show("TUYỆT VỜI!\n\n1. Nội dung đã được giải mã an toàn.\n2. Chữ ký xác thực ĐÚNG là của người gửi.", "Kết quả kiểm tra");
+                }
+                else
+                {
+                    MessageBox.Show("CẢNH BÁO!\n\nGiải mã được nội dung nhưng Chữ ký KHÔNG khớp.\nCó thể dữ liệu đã bị sửa đổi!", "Kết quả kiểm tra");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi xử lý gói tin: " + ex.Message);
+            }
+        }
+
+        private BigInteger ChooseE(BigInteger phi)
+        {
+            BigInteger e = 65537;
+            if (e >= phi) e = 3;
+
+            while (BigInteger.GreatestCommonDivisor(e, phi) != 1)
+            {
+                e += 2;
+            }
+            return e;
+        }
+
+        private BigInteger ModInverse(BigInteger a, BigInteger m)
+        {
+            BigInteger m0 = m;
+            BigInteger y = 0, x = 1;
+            if (m == 1) return 0;
+
+            while (a > 1)
+            {
+                BigInteger q = a / m;
+                BigInteger t = m;
+
+                m = a % m;
+                a = t;
+                t = y;
+
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0) x += m0;
+
+            return x;
+        }
+    }
+}
